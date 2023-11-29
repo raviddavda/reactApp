0bedb643d6198d7d7cc3bec6829a0fefac6ba6f3
raviddavda <139677733+raviddavda@users.noreply.github.com>
2023-11-29 12:42:35 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </Provider>
